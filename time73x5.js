4a8c08bb358b159f39256f195cc327bd9647248b
gbsgxyz <72190554+gbsgxyz@users.noreply.github.com>
2020-10-25 12:45:45 +0700

    Update time73x5.js

--- time73x5.js
+++ time73x5.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 10, 2020 10:10:00").getTime();
+var countDownDate = new Date("Oct 30, 2020 10:10:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("time73x5").innerHTML = countDownDate;
+    document.getElementById("time73x5").innerHTML = "Chzi Luon";
   }
 }, 1000);
